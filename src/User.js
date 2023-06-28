38d8c7e955143d44bc0c76c2613d0b7b48abd8a3
SushantWebDeck <sushantchouhan90@gmail.com>
2023-06-28 14:02:32 +0530

    add to cart design

---
+++ src/User.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const User=(props)=>{
+    const {data} = props;
+    console.log(props.data.name, props.data.age)
+    return(
+        <div>
+            <h1>User component</h1>
+            <h4>{data.name}</h4>
+            <h4>{data.age}</h4>
+        </div>
+    )
+}
+
+export default User;
